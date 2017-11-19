922516418a29634f040ae74948a9e578dd49ca47
Khang Tran <piskael03@gmail.com>
2017-11-18 20:46:01 -0500

    implement vehicle form

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -9,27 +9,35 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from "./components/vehicle-form/vehicle-form.component";
+import { MakeService } from "./services/make.service";
 
-@NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
-    ],
-    imports: [
-        CommonModule,
-        HttpModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
-})
+@
+    NgModule({
+        declarations: [
+            AppComponent,
+            NavMenuComponent,
+            CounterComponent,
+            FetchDataComponent,
+            HomeComponent,
+            VehicleFormComponent
+        ],
+        imports: [
+            CommonModule,
+            HttpModule,
+            FormsModule,
+            RouterModule.forRoot([
+                { path: '', redirectTo: 'home', pathMatch: 'full' },
+                { path: 'vehicles/new', component: VehicleFormComponent },
+                { path: 'home', component: HomeComponent },
+                { path: 'counter', component: CounterComponent },
+                { path: 'fetch-data', component: FetchDataComponent },
+                { path: '**', redirectTo: 'home' }
+            ])
+        ],
+        providers: [
+            MakeService
+        ]
+    })
 export class AppModuleShared {
 }
